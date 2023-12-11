7b2e86816c60cdb94969f33ebfa458bc537ef898
pedromlsr <pedrodesarodrigues@gmail.com>
2023-12-11 20:38:53 -0300

    transaction table

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,41 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>12/11/2023</td>
+              <td></td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>12/09/2023</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
